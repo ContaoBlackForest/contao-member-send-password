67e823da512abea72973bcc2f266fa42912eb6f3
Sven Baumann <baumann.sv@gmail.com>
2019-11-15 00:04:43 +0100

    Add the language

---
+++ src/Resources/contao/languages/de/tl_nc_notification.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of contao-member-send-password.
+ *
+ * (c) 2016-2019 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-member-send-password
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  20116-2019 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-member-send-password/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['cb_member_send_password'] = [
+    'Mitglied neues Password',
+    'Dieser Nachrichtenart wird verschickt, wenn dem Mitglied ein neues Password gesendet werden soll.'
+];
---
+++ src/Resources/contao/languages/en/tokens.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of contao-member-send-password.
+ *
+ * (c) 2016-2019 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-member-send-password
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  20116-2019 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-member-send-password/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['cb_member_send_password']['member_*']         =
+    'All the member data.';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['cb_member_send_password']['member_label_*']   =
+    'All the member labels.';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['cb_member_send_password']['new_password']     =
+    'The new password.';
+$GLOBALS['TL_LANG']['NOTIFICATION_CENTER_TOKEN']['cb_member_send_password']['data']             =
+    'All available data.';
