c275eb293b53969a39a6ef8fab2662763f8414de
Sven Baumann <baumann.sv@gmail.com>
2019-11-14 15:48:24 +0100

    Change the php header

--- Resources/contao/config/autoload.php
+++ Resources/contao/config/autoload.php
@@ -1,15 +1,21 @@
 <?php
 
 /**
- * Copyright © ContaoBlackForest
+ * This file is part of contao-member-send-password.
  *
- * @package   contao-member-send-password
- * @author    Sven Baumann <baumann.sv@gmail.com>
- * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
- * @license   GNU/LGPL
- * @copyright Copyright 2014-2016 ContaoBlackForest
+ * (c) 2016-2019 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-member-send-password
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  20116-2019 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-member-send-password/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
  */
-
 /**
  * Add template files
  */
