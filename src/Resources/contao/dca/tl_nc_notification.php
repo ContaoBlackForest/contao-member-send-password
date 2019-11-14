a692324861498afc4e67ae1e84a3df7a136b27e2
Sven Baumann <baumann.sv@gmail.com>
2019-11-14 23:55:07 +0100

    Add the data container configuration for the notification center

---
+++ src/Resources/contao/dca/tl_nc_notification.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of contao-member-send-password.
+ *
+ * (c) 2016-2019 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-member-send-password
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  20116-2019 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-member-send-password/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+$GLOBALS['TL_DCA']['tl_nc_notification']['palettes']['cb_member_send_password'] =
+    '{title_legend},title,type;' .
+    '{config_legend},flatten_delimiter;' .
+    '{templates_legend:hide},templates';
