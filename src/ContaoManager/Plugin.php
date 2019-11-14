83561601bb89596f67c51bfa35c2994bb78aca9f
Sven Baumann <baumann.sv@gmail.com>
2019-11-14 16:17:07 +0100

    Support the contao managed edition

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of contao-member-send-password.
+ *
+ * (c) 2016-2019 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-member-send-password
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  20116-2019 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-member-send-password/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+declare(strict_types=1);
+
+namespace ContaoBlackForest\MemberSendPasswordBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoBlackForest\MemberSendPasswordBundle\BlackForestMemberSendPasswordBundle;
+
+/**
+ * The contao manager plugin.
+ */
+final class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(BlackForestMemberSendPasswordBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class
+                    ]
+                )
+        ];
+    }
+}
