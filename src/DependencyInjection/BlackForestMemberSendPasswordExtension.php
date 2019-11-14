9ee300856e65b3126e1e43126f1ead6abe138b46
Sven Baumann <baumann.sv@gmail.com>
2019-11-14 16:11:57 +0100

    Create the bundle

---
+++ src/DependencyInjection/BlackForestMemberSendPasswordExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of contao-member-send-password.
+ *
+ * (c) 2016-2019 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-member-send-password
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  20116-2019 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-member-send-password/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+namespace ContaoBlackForest\MemberSendPasswordBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * The extension.
+ */
+final class BlackForestMemberSendPasswordExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    }
+}
